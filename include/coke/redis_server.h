70946a8ea1b644829d047a13ce1e4c0c1e454d5a
kedixa <1204837541@qq.com>
2023-10-06 21:28:26 +0800

    feature: RedisServer

---
+++ include/coke/redis_server.h
@@ -0,0 +1,36 @@
+#ifndef COKE_REDIS_SERVER_H
+#define COKE_REDIS_SERVER_H
+
+#include "coke/detail/task.h"
+#include "coke/server_common.h"
+
+#include "workflow/WFRedisServer.h"
+#include "workflow/RedisMessage.h"
+
+namespace coke {
+
+using RedisRequest = protocol::RedisRequest;
+using RedisResponse = protocol::RedisResponse;
+using RedisValue = protocol::RedisValue;
+using RedisServerContext = ServerContext<WFNetworkTask<RedisRequest, RedisResponse>>;
+using RedisReplyResult = NetworkReplyResult;
+
+struct RedisServerParams : public ServerParams {
+    RedisServerParams() : ServerParams(REDIS_SERVER_PARAMS_DEFAULT) { }
+    ~RedisServerParams() = default;
+};
+
+class RedisServer : public BasicServer<RedisRequest, RedisResponse> {
+public:
+    RedisServer(const RedisServerParams &params, ProcessorType co_proc)
+        : BasicServer<RedisRequest, RedisResponse>(params, std::move(co_proc))
+    { }
+
+    RedisServer(ProcessorType co_proc)
+        : BasicServer<RedisRequest, RedisResponse>(RedisServerParams(), std::move(co_proc))
+    { }
+};
+
+} // namespace coke
+
+#endif // COKE_REDIS_SERVER_H
