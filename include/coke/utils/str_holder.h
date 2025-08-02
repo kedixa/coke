19f127323e65cb98a0f25b064e0002fe85b8cc28
kedixa <1204837541@qq.com>
2025-08-02 14:58:16 +0800

    feature: make shv from iterators

--- include/coke/utils/str_holder.h
+++ include/coke/utils/str_holder.h
@@ -39,12 +39,12 @@ public:
 
     StrHolder(const char *s) : value(std::in_place_type_t<std::string>(), s) {}
 
-    StrHolder(const StrHolder &other)     = default;
+    StrHolder(const StrHolder &other) = default;
     StrHolder(StrHolder &&other) noexcept = default;
 
     ~StrHolder() = default;
 
-    StrHolder &operator=(const StrHolder &other)     = default;
+    StrHolder &operator=(const StrHolder &other) = default;
     StrHolder &operator=(StrHolder &&other) noexcept = default;
 
     StrHolder &operator=(const std::string_view &sv) noexcept
@@ -81,7 +81,7 @@ public:
         return std::holds_alternative<std::string_view>(value);
     }
 
-    std::string_view get_view() const noexcept
+    std::string_view get_view() const
     {
         return std::get<std::string_view>(value);
     }
@@ -118,18 +118,32 @@ private:
     std::variant<std::string, std::string_view> value;
 };
 
+template<typename T>
+concept IndirectlyToStr =
+    std::indirectly_readable<T> &&
+    std::constructible_from<StrHolder, std::iter_value_t<T>>;
+
 using StrHolderVec = std::vector<StrHolder>;
 
-inline auto operator"" _sv(const char *str, std::size_t len)
+inline auto operator""_sv(const char *str, std::size_t len)
 {
     return std::string_view(str, len);
 }
 
-inline auto operator"" _svh(const char *str, std::size_t len)
+inline auto operator""_svh(const char *str, std::size_t len)
 {
     return StrHolder(std::string_view(str, len));
 }
 
+/**
+ * @brief Make StrHolderVec from strs.
+ *
+ * ```cpp
+ * std::string str;
+ * std::string_view sv;
+ * auto vec = coke::make_shv("hello", str, sv);
+ * ```
+ */
 template<typename... Strs>
     requires (std::constructible_from<StrHolder, Strs &&> && ...)
 StrHolderVec make_shv(Strs &&...strs)
@@ -137,6 +151,55 @@ StrHolderVec make_shv(Strs &&...strs)
     StrHolderVec v;
     v.reserve(sizeof...(strs));
     (v.emplace_back(std::forward<Strs>(strs)), ...);
+    return v;
+}
+
+/**
+ * @brief Make StrHolderVec from iterator range [begin, end).
+ *
+ * ```cpp
+ * std::vector<std::string> vstr = ...;
+ * auto vec = coke::make_shv(vstr.begin(), vstr.end());
+ * ```
+ */
+template<IndirectlyToStr BeginIt, IndirectlyToStr EndIt>
+StrHolderVec make_shv(BeginIt begin, EndIt end)
+{
+    StrHolderVec v;
+
+    if constexpr (std::random_access_iterator<BeginIt> &&
+                  std::random_access_iterator<EndIt>)
+    {
+        v.reserve(std::distance(begin, end));
+    }
+
+    v.assign(begin, end);
+    return v;
+}
+
+/**
+ * @brief Make StrHolderVec from iterator range [begin, end), store strings
+ *        as std::string_view.
+ *
+ * ```cpp
+ * std::vector<std::string> vstr = ...;
+ * auto vec = coke::make_shv_view(vstr.begin(), vstr.end());
+ * ```
+ */
+template<IndirectlyToStr BeginIt, IndirectlyToStr EndIt>
+StrHolderVec make_shv_view(BeginIt begin, EndIt end)
+{
+    StrHolderVec v;
+
+    if constexpr (std::random_access_iterator<BeginIt> &&
+                  std::random_access_iterator<EndIt>)
+    {
+        v.reserve(std::distance(begin, end));
+    }
+
+    for (auto it = begin; it != end; ++it)
+        v.push_back(std::string_view(*it));
+
     return v;
 }
 
