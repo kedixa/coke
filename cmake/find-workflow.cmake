c7211329765fc02419192d0a50ab1963279d1f85
kedixa <1204837541@qq.com>
2025-12-13 15:00:12 +0800

    build: add find-workflow.cmake

--- cmake/find-workflow.cmake
+++ cmake/find-workflow.cmake
@@ -1,7 +1,33 @@
-# Find workflow library and create target coke-workflow-static,
-# coke-workflow-shared.
+# Find workflow library and create coke::workflow-static and/or
+# coke::workflow-shared.
 
-if (NOT TARGET coke-workflow-static)
+if (TARGET coke::workflow-static OR TARGET coke::workflow-shared)
+    # Target has been created, nothing todo.
+elseif (TARGET workflow-static OR TARGET workflow-shared)
+    # Maybe in subdirectory mode, create alias.
+
+    if (TARGET workflow-static)
+        add_library(coke::workflow-static ALIAS workflow-static)
+    endif ()
+
+    if (TARGET workflow-shared)
+        add_library(coke::workflow-shared ALIAS workflow-shared)
+    endif ()
+else ()
+    # In config mode, find workflow and create target.
+
+    if (NOT WORKFLOW_INCLUDE_DIR OR NOT WORKFLOW_LIB_DIR)
+        find_package(workflow REQUIRED)
+    endif ()
+
+    if (NOT TARGET OpenSSL::SSL OR NOT TARGET OpenSSL::Crypto)
+        find_package(OpenSSL)
+    endif ()
+
+    if (NOT TARGET Threads)
+        find_package(Threads)
+    endif ()
+
     find_library(
         COKE_STATIC_WORKFLOW_LIBRARY
         NAMES
@@ -10,18 +36,17 @@ if (NOT TARGET coke-workflow-static)
     )
 
     if (COKE_STATIC_WORKFLOW_LIBRARY)
-        add_library(coke-workflow-static STATIC IMPORTED GLOBAL)
-        set_target_properties(coke-workflow-static
+        add_library(coke::workflow-static STATIC IMPORTED GLOBAL)
+        set_target_properties(coke::workflow-static
             PROPERTIES
                 IMPORTED_LOCATION ${COKE_STATIC_WORKFLOW_LIBRARY}
                 INTERFACE_INCLUDE_DIRECTORIES ${WORKFLOW_INCLUDE_DIR}
+                INTERFACE_LINK_LIBRARIES "OpenSSL::SSL;OpenSSL::Crypto;Threads::Threads"
         )
 
-        message("Find static Workflow: " ${COKE_STATIC_WORKFLOW_LIBRARY})
+        message("Find static workflow: " ${COKE_STATIC_WORKFLOW_LIBRARY})
     endif ()
-endif ()
 
-if (NOT TARGET coke-workflow-shared)
     find_library(
         COKE_SHARED_WORKFLOW_LIBRARY
         NAMES
@@ -30,30 +55,14 @@ if (NOT TARGET coke-workflow-shared)
     )
 
     if (COKE_SHARED_WORKFLOW_LIBRARY)
-        add_library(coke-workflow-shared SHARED IMPORTED GLOBAL)
-        set_target_properties(coke-workflow-shared
+        add_library(coke::workflow-shared SHARED IMPORTED GLOBAL)
+        set_target_properties(coke::workflow-shared
             PROPERTIES
                 IMPORTED_LOCATION ${COKE_SHARED_WORKFLOW_LIBRARY}
                 INTERFACE_INCLUDE_DIRECTORIES ${WORKFLOW_INCLUDE_DIR}
+                INTERFACE_LINK_LIBRARIES "OpenSSL::SSL;OpenSSL::Crypto;Threads::Threads"
         )
 
-        message("Find shared Workflow: " ${COKE_SHARED_WORKFLOW_LIBRARY})
-    endif ()
-endif ()
-
-if (NOT TARGET coke-workflow)
-    get_target_property(coke_target_type coke::coke TYPE)
-    if (coke_target_type STREQUAL "STATIC_LIBRARY")
-        set(coke_is_static TRUE)
-    else ()
-        set(coke_is_static FALSE)
-    endif ()
-
-    if (TARGET coke-workflow-static AND coke_is_static)
-        add_library(coke-workflow ALIAS coke-workflow-static)
-    elseif (TARGET coke-workflow-shared)
-        add_library(coke-workflow ALIAS coke-workflow-shared)
-    else ()
-        message(FATAL_ERROR "Workflow Not Found")
+        message("Find shared workflow: " ${COKE_SHARED_WORKFLOW_LIBRARY})
     endif ()
 endif ()
