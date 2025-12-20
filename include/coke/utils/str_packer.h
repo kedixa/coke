5fd933dc5379d3576b906153aa2e543c60fb1c7d
kedixa <1204837541@qq.com>
2025-12-20 14:17:47 +0800

    feature: StrPacker support move and extend

--- include/coke/utils/str_packer.h
+++ include/coke/utils/str_packer.h
@@ -34,60 +34,98 @@ class StrPacker {
 public:
     StrPacker() = default;
 
-    StrPacker(const StrPacker &) = delete;
-    StrPacker(StrPacker &&) = delete;
+    StrPacker(StrPacker &&p) noexcept : strs(std::move(p.strs)) { p.clear(); }
 
+    StrPacker &operator=(StrPacker &&p) noexcept
+    {
+        if (this != &p) {
+            strs = std::move(p.strs);
+            p.clear();
+        }
+
+        return *this;
+    }
+
+    StrPacker(const StrPacker &) = delete;
     StrPacker &operator=(const StrPacker &) = delete;
-    StrPacker &operator=(StrPacker &&) = delete;
 
     ~StrPacker() = default;
 
-    StrPacker &append(const char *str) { return append(str, std::strlen(str)); }
+    StrPacker &append(const char *str)
+    {
+        return append_impl(str, std::strlen(str));
+    }
 
     StrPacker &append(const std::string &str)
     {
-        return append(str.data(), str.size());
+        return append_impl(str.data(), str.size());
     }
 
     StrPacker &append(std::string_view sv)
     {
-        return append(sv.data(), sv.size());
+        return append_impl(sv.data(), sv.size());
     }
 
     StrPacker &append(const char *str, std::size_t len)
     {
-        if (len > 0)
-            buf(len).append(str, len);
+        return append_impl(str, len);
+    }
 
-        return *this;
+    StrPacker &append(std::string &&str)
+    {
+        return append_nocopy_impl(std::move(str));
     }
 
     StrPacker &append_nocopy(const char *str)
     {
-        return append_nocopy(str, std::strlen(str));
+        return append_nocopy_impl(str, std::strlen(str));
     }
 
     StrPacker &append_nocopy(const std::string &str)
     {
-        return append_nocopy(str.data(), str.size());
+        return append_nocopy_impl(str.data(), str.size());
     }
 
     StrPacker &append_nocopy(std::string_view sv)
     {
-        return append_nocopy(sv.data(), sv.size());
+        return append_nocopy_impl(sv.data(), sv.size());
     }
 
     StrPacker &append_nocopy(const char *str, std::size_t len)
     {
-        if (len <= SMALL_HINT)
-            return append(str, len);
+        return append_nocopy_impl(str, len);
+    }
 
-        strs.emplace_back(std::string_view(str, len));
+    StrPacker &append_nocopy(std::string &&str)
+    {
+        return append_nocopy_impl(std::move(str));
+    }
+
+    StrPacker &extend_copy(const StrPacker &pk)
+    {
+        for (const StrHolder &sh : pk.strs)
+            append(sh.as_view());
+
         return *this;
     }
 
-    std::size_t strs_count() const { return strs.size(); }
+    StrPacker &extend_nocopy(StrPacker &&pk)
+    {
+        for (StrHolder &sh : pk.strs) {
+            if (sh.holds_view())
+                append_nocopy(sh.get_view());
+            else
+                append_nocopy(std::move(sh.get_string()));
+        }
 
+        pk.clear();
+        return *this;
+    }
+
+    /**
+     * @brief Merge the internal strs.
+     * @param max max should >= 1. The max number of strs after merging.
+     */
     void merge(std::size_t max)
     {
         if (strs.size() <= max)
@@ -99,11 +137,65 @@ public:
         return do_merge((max - 1) / 2);
     }
 
-    const std::vector<StrHolder> &get_strs() const { return strs; }
+    std::size_t strs_count() const noexcept { return strs.size(); }
 
+    /**
+     * @brief Get the total bytes of all strs.
+     * @attention The complexity is O(n).
+     */
+    std::size_t total_bytes() const noexcept
+    {
+        std::size_t n = 0;
+
+        for (const auto &sh : strs)
+            n += sh.size();
+
+        return n;
+    }
+
+    /**
+     * @brief Get the internal strs.
+     * @attention If the strs are modified, clear() should be called to reset
+     *            the internal state before further usage.
+     */
+    std::vector<StrHolder> &get_strs() noexcept { return strs; }
+
+    const std::vector<StrHolder> &get_strs() const noexcept { return strs; }
+
+    /**
+     * @brief Clear the internal strs.
+     */
     void clear() { strs.clear(); }
 
 private:
+    StrPacker &append_impl(const char *str, std::size_t len)
+    {
+        if (len > 0)
+            buf(len).append(str, len);
+
+        return *this;
+    }
+
+    StrPacker &append_nocopy_impl(const char *str, std::size_t len)
+    {
+        if (len <= SMALL_HINT)
+            append_impl(str, len);
+        else
+            strs.emplace_back(std::string_view(str, len));
+
+        return *this;
+    }
+
+    StrPacker &append_nocopy_impl(std::string &&str)
+    {
+        if (str.size() <= SMALL_HINT)
+            append_impl(str.data(), str.size());
+        else
+            strs.emplace_back(std::move(str));
+
+        return *this;
+    }
+
     std::string &buf(std::size_t hint)
     {
         if (strs.empty() || strs.back().holds_view()) {
